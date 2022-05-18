1d6c7199270e2dd9a96486c8b635e250c027d115
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-05-19 00:52:24 +0600

    db connected

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.50dwp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    console.log('db connected')
+    client.close();
+});
+
 
 app.get('/', async (req, res) => {
     res.send('Hello from TODO');
